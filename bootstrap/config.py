d39d238e4f96567d7566a75d83237d7b9db04adb
MarkKoz <KozlovMark@gmail.com>
2020-10-25 21:48:43 -0700

    Create function to load config for a single module
    Such function is useful when extensions are dynamically loaded via a
    command. It can be called right after a new extension is loaded. It's
    more ideal than `load_config` since it avoids going through all loaded
    extensions.

--- bootstrap/config.py
+++ bootstrap/config.py
@@ -1,7 +1,7 @@
 import logging
 from pathlib import Path
 from types import ModuleType
-from typing import Iterator, Optional
+from typing import Optional
 
 import yaml
 
@@ -23,13 +23,6 @@ def _get_config_object(module: ModuleType) -> Optional[object]:
             return obj
 
 
-def _get_config_objects(bot) -> Iterator[object]:
-    """Return the config objects defined in all loaded extensions for `bot`."""
-    for module in bot.extensions.values():
-        if obj := _get_config_object(module):
-            yield obj
-
-
 def _read_config(path: str) -> dict:
     """
     Read the YAML config file at `path` and return its deserialised content.
@@ -44,6 +37,37 @@ def _read_config(path: str) -> dict:
     raise FileNotFoundError(f"No config found at path {path!r}")
 
 
+def _load_module_config(module: ModuleType, user_config: dict) -> None:
+    """Populate attributes of the config object in `module` with values from `user_config`."""
+    if not (obj := _get_config_object(module)):
+        return
+
+    # TODO: throw a warning for unset attributes (i.e. only have a type annotation)?
+    for name in vars(obj):
+        if name.startswith("_"):
+            continue  # Skip private attributes.
+
+        # Split to get the suffix, which may determine where to find the config value.
+        split_name = name.rsplit("_", 1)
+
+        try:
+            if len(split_name) > 1 and (key := _SUFFIX_KEYS.get(split_name[1])):
+                # Look for the value under the corresponding guild key for the suffix.
+                guild = user_config["guild"] or {}  # Avoids TypeErrors when it's None.
+                category = guild[key] or {}
+                new_value = category[split_name[0]]
+            else:
+                # All other attributes are considered specific to that extension.
+                category = user_config[obj.__module__] or {}
+                new_value = category[name]
+        except KeyError:
+            _log.debug(f"No config value found for {obj.__module__}.{name}; keep the default")
+            pass
+        else:
+            _log.debug(f"Set {obj.__module__}.{name} to {new_value!r}")
+            setattr(obj, name, new_value)
+
+
 def load_config(bot, path: str = _CONFIG_PATH) -> None:
     """
     Populate attributes of all config objects with values from the config file at `path`.
@@ -51,28 +75,15 @@ def load_config(bot, path: str = _CONFIG_PATH) -> None:
     Raise FileNotFoundError if the config file does not exist.
     """
     user_config = _read_config(path)
-    for obj in _get_config_objects(bot):
-        # TODO: throw a warning for unset attributes (i.e. only have a type annotation)?
-        for name in vars(obj):
-            if name.startswith("_"):
-                continue  # Skip private attributes.
+    for module in bot.extensions.values():
+        _load_module_config(module, user_config)
 
-            # Split to get the suffix, which may determine where to find the config value.
-            split_name = name.rsplit("_", 1)
 
-            try:
-                if len(split_name) > 1 and (key := _SUFFIX_KEYS.get(split_name[1])):
-                    # Look for the value under the corresponding guild key for the suffix.
-                    guild = user_config["guild"] or {}  # Avoids TypeErrors when it's None.
-                    category = guild[key] or {}
-                    new_value = category[split_name[0]]
-                else:
-                    # All other attributes are considered specific to that extension.
-                    category = user_config[obj.__module__] or {}
-                    new_value = category[name]
-            except KeyError:
-                _log.debug(f"No config value found for {obj.__module__}.{name}; keep the default")
-                pass
-            else:
-                _log.debug(f"Set {obj.__module__}.{name} to {new_value!r}")
-                setattr(obj, name, new_value)
+def load_module_config(module: ModuleType, path: str = _CONFIG_PATH) -> None:
+    """
+    Populate attributes of the config object in `module` with values from the config file at `path`.
+
+    Raise FileNotFoundError if the config file does not exist.
+    """
+    user_config = _read_config(path)
+    _load_module_config(module, user_config)
