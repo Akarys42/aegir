f2853f331124d83049d2444e52ab7eac27787750
MarkKoz <KozlovMark@gmail.com>
2020-10-25 22:14:58 -0700

    Raise KeyError if config value not found and default is missing

--- bootstrap/config.py
+++ bootstrap/config.py
@@ -38,12 +38,19 @@ def _read_config(path: str) -> dict:
 
 
 def _load_module_config(module: ModuleType, user_config: dict) -> None:
-    """Populate attributes of the config object in `module` with values from `user_config`."""
+    """
+    Populate attributes of the config object in `module` with values from `user_config`.
+
+    Raise KeyError if a configured value is not found for an attribute without a default value.
+    """
     if not (obj := _get_config_object(module)):
         return
 
-    # TODO: throw a warning for unset attributes (i.e. only have a type annotation)?
-    for name in vars(obj):
+    annotations = set(obj.__annotations__)
+    attributes = set(vars(obj))
+    missing_defaults = annotations - attributes
+
+    for name in attributes | annotations:
         if name.startswith("_"):
             continue  # Skip private attributes.
 
@@ -61,8 +68,10 @@ def _load_module_config(module: ModuleType, user_config: dict) -> None:
                 category = user_config[obj.__module__] or {}
                 new_value = category[name]
         except KeyError:
-            _log.debug(f"No config value found for {obj.__module__}.{name}; keep the default")
-            pass
+            qualified = f"{obj.__module__}.{name}"
+            if name in missing_defaults:
+                raise KeyError(f"No config value found for {qualified} and no default specified")
+            _log.debug(f"No config value found for {qualified}; keeping the default")
         else:
             _log.debug(f"Set {obj.__module__}.{name} to {new_value!r}")
             setattr(obj, name, new_value)
@@ -73,6 +82,7 @@ def load_config(bot, path: str = _CONFIG_PATH) -> None:
     Populate attributes of all config objects with values from the config file at `path`.
 
     Raise FileNotFoundError if the config file does not exist.
+    Raise KeyError if a configured value is not found for an attribute without a default value.
     """
     user_config = _read_config(path)
     for module in bot.extensions.values():
@@ -84,6 +94,7 @@ def load_module_config(module: ModuleType, path: str = _CONFIG_PATH) -> None:
     Populate attributes of the config object in `module` with values from the config file at `path`.
 
     Raise FileNotFoundError if the config file does not exist.
+    Raise KeyError if a configured value is not found for an attribute without a default value.
     """
     user_config = _read_config(path)
     _load_module_config(module, user_config)
