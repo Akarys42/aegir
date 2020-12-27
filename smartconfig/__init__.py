a6aa995b193a05603a1e3fc96bac44436d0bfdf8
Matteo Bertucci <matteobertucci2004@gmail.com>
2020-12-27 17:52:00 +0100

    Move the imports at the top of __init__.py

--- smartconfig/__init__.py
+++ smartconfig/__init__.py
@@ -1,5 +1,16 @@
 """A minimal package to support configuration files without any additional work."""
 
+from smartconfig.config_files import load_config_file
+from smartconfig.entry import ConfigEntry
+from smartconfig.exceptions import (
+    ConfigurationKeyError,
+    DuplicateConfiguration,
+    InvalidOperation,
+    MalformedYamlFile,
+    SmartconfigException
+)
+from smartconfig.typehints import EntryType
+
 __version__ = "0.0.0-dev1"
 
 __all__ = (
@@ -13,14 +24,3 @@ __all__ = (
     # Typehinting
     "EntryType",
 )
-
-from smartconfig.config_files import load_config_file
-from smartconfig.entry import ConfigEntry
-from smartconfig.exceptions import (
-    ConfigurationKeyError,
-    DuplicateConfiguration,
-    InvalidOperation,
-    MalformedYamlFile,
-    SmartconfigException
-)
-from smartconfig.typehints import EntryType
