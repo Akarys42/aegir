730979acc41e00f468126005f0ce1fa6bb146131
Matteo Bertucci <matteobertucci2004@gmail.com>
2020-12-29 17:33:18 +0100

    Move code to the metaclass and use name mangling
    Co-authored-by: MarkKoz <KozlovMark@gmail.com>

--- smartconfig/entry.py
+++ smartconfig/entry.py
@@ -12,6 +12,10 @@ class _ConfigEntryMeta(type):
     Note: Using this metaclass outside of the library is currently not supported.
     """
 
+    __path: str
+    __path_override: Optional[str]
+    __defined_entries: List[str]
+
     def __getattribute__(cls, item: str) -> Optional[Any]:
         """
         Special attribute lookup function.
@@ -28,10 +32,10 @@ class _ConfigEntryMeta(type):
         if item.startswith('_'):
             return super().__getattribute__(item)
 
-        if item not in registry.global_configuration[cls._path]:
-            raise ConfigurationKeyError(f"Entry {cls._path} doesn't define any {item} entry.")
+        if item not in registry.global_configuration[cls.__path]:
+            raise ConfigurationKeyError(f"Entry {cls.__path} doesn't define any {item} entry.")
 
-        return registry.global_configuration[cls._path][item]
+        return registry.global_configuration[cls.__path][item]
 
     def __new__(cls, name: str, bases: Tuple[type, ...], dict_: Dict[str, Any], path: Optional[str] = None) -> type:
         """
@@ -41,21 +45,64 @@ class _ConfigEntryMeta(type):
             path: Custom path used for this entry instead of the module name.
         """
         names = chain(dict_.keys(), dict_.get('__annotations__', {}).keys())
-        dict_["_defined_entries"] = {name for name in names if not name.startswith('_')}
+        dict_[f"{cls.__name__}__defined_entries"] = {name for name in names if not name.startswith('_')}
 
-        dict_["_path_override"] = path
+        dict_[f"{cls.__name__}__path_override"] = path
 
         return super().__new__(cls, name, bases, dict_)
 
+    def _register_entry(cls) -> None:
+        """Set the `__path` attribute and register the entry."""
+        cls.__path = cls.__path_override or cls.__module__
+        if cls.__path in registry.configuration_for_module:
+            raise DuplicateConfiguration(f"An entry at {cls.__path} already exists.")  # TODO: Add an FAQ link.
+
+        configuration = {
+            key: value for key, value in cls.__dict__.items() if not key.startswith('_')
+        }
+
+        if cls.__path not in registry.global_configuration:
+            registry.global_configuration[cls.__path] = configuration
+        # We already have some overrides for this path.
+        else:
+            for key, value in configuration.items():
+                # We only write values that aren't already defined.
+                if key not in registry.global_configuration[cls.__path]:
+                    registry.global_configuration[cls.__path][key] = value
+
+        registry.configuration_for_module[cls.__path] = cls
+
+    def _check_undefined_entries(cls) -> None:
+        """Raise `ConfigurationKeyError` if any attribute doesn't have a concrete value."""
+        for attribute in cls.__defined_entries:
+            if attribute not in registry.global_configuration[cls.__path]:
+                raise ConfigurationKeyError(f"Entry {attribute} isn't defined.")
+
+    def __init__(cls, name: str, bases: Tuple[type, ...], dict_: Dict[str, Any], path: Optional[str] = None):
+        """
+        Initialize the new entry.
+
+        Raises:
+            DuplicateConfiguration: An entry is already registered for this path, use `__path_override`.
+            ConfigurationKeyError: An attribute doesn't have a concrete value.
+        """
+        super().__init__(name, bases, dict_)
+
+        cls._register_entry()
+        cls._check_undefined_entries()
+
     def __repr__(cls) -> str:
         """Return a short representation of the entry."""
-        return f"<{cls.__name__} entry at {cls._path}>"
+        if hasattr(cls, '__path'):
+            return f"<Entry {cls.__name__} at {cls.__path}>"
+        else:
+            return f"<Entry {cls.__name__}>"
 
     def __eq__(cls, other: Any) -> bool:
         """Return true if this entry and the other point to the same path."""
         if not isinstance(other, _ConfigEntryMeta):
             return NotImplemented
-        return cls._path == other._path
+        return cls.__path == other.__path
 
 
 class ConfigEntry(metaclass=_ConfigEntryMeta):
@@ -68,43 +115,6 @@ class ConfigEntry(metaclass=_ConfigEntryMeta):
 
     The default path used by an entry is the name of the module it is defined in, or the `path` metaclass argument.
     """
-
-    _path: str
-    _path_override: Optional[str]
-    _defined_entries: List[str]
-
-    @classmethod
-    def _register_entry(cls) -> None:
-        """Set the `_path` attribute and register the entry."""
-        cls._path = cls._path_override or cls.__module__
-        if cls._path in registry.configuration_for_module:
-            raise DuplicateConfiguration(f"An entry at {cls._path} already exists.")  # TODO: Add an FAQ link.
-
-        registry.global_configuration[cls._path] = {
-            key: value for key, value in cls.__dict__.items() if not key.startswith('_')
-        }
-
-        registry.configuration_for_module[cls._path] = cls
-
-    @classmethod
-    def _check_undefined_entries(cls) -> None:
-        """Raise `ConfigurationKeyError` if any attribute doesn't have a concrete value."""
-        for attribute in cls._defined_entries:
-            if attribute not in registry.global_configuration[cls._path]:
-                raise ConfigurationKeyError(f"Entry {attribute} isn't defined.")
-
-    def __init_subclass__(cls, **kwargs) -> None:
-        """
-        Initialize the new entry.
-
-        Raises:
-            DuplicateConfiguration: An entry is already registered for this path, use `_path_override`.
-            ConfigurationKeyError: An attribute doesn't have a concrete value.
-        """
-        super().__init_subclass__(**kwargs)
-
-        cls._register_entry()
-        cls._check_undefined_entries()
 
     def __init__(self) -> NoReturn:
         """Raises `InvalidOperation` as creating instances isn't allowed."""
