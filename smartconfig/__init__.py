b91712061c5f80dc6e5a8ca694d04ea36a092d0d
Matteo Bertucci <matteobertucci2004@gmail.com>
2021-01-17 17:46:21 +0100

    Rename config_files.py to file.py

--- smartconfig/__init__.py
+++ smartconfig/__init__.py
@@ -1,6 +1,5 @@
 """A minimal package to support configuration files without any additional work."""
 
-from smartconfig.config_files import load
 from smartconfig.entry import ConfigEntry
 from smartconfig.exceptions import (
     ConfigurationKeyError,
@@ -8,6 +7,7 @@ from smartconfig.exceptions import (
     PathConflict,
     SmartconfigException
 )
+from smartconfig.file import load
 from smartconfig.typehints import EntryType
 
 __version__ = "0.0.0-dev1"
